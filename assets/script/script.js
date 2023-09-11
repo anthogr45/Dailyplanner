92822a73879ffe1eb59dd3af071cfceeb0fdcfe4
AnthoG <anthony.dswg@gmail.com>
2023-09-11 14:26:39 -0400

    Block Color Changes done

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,133 +5,254 @@ var today = dayjs().format('ddd, MMM D, YYYY h:mm A');
 console.log(today);
 $(wdDate).text(today);
 
-var currentTime = dayjs().format('HH');
+var clockTime = dayjs().format('HH');
 
-if (currentTime < 4) {
+var currentTime = parseInt(clockTime);
+
+// var typee = typeof currentTime;
 console.log(currentTime);
-}
+
+// if (currentTime < 4) {
+// console.log(currentTime);
+// }
 // blockColor ();
 
 
-var block9AM = $('#hour-9');
+var block9AM = $('#9');
+var block10AM = $('#10');
+var block11AM = $('#11');
+var block12PM = $('#12');
+var block1PM = $('#13');
+var block2PM = $('#14');
+var block3PM = $('#15');
+var block4PM = $('#16');
+var block5PM = $('#17');
+
 var block9 = 9;
 var block9AMTXT = $('#txt9');
 var block9AMBtn = $("#btn9AM");
 
-var papa
+// var papa
 
-block9AMBtn.on('click', function() {
-    alert('Hello World');
+// block9AMBtn.on('click', function() {
+//     alert('Hello World');
  
-  papa = this.parentNode.id;
-    // papa = block9AMBtn.parent().id
-    var typee = typeof papa;
-  console.log(typee);
+//   papa = this.parentNode.id;
+//     // papa = block9AMBtn.parent().id
+//     var typee = typeof papa;
+//   console.log(typee);
 
-  if (papa < 10) {
-    console.log("papa boy");
-  }
+//   if (papa < 10) {
+//     console.log("papa boy");
+//   }
 
-  });
-// console.log(block9AMBtn.parent);
-// block9AM.attr('class', 'row time-block future' )
+//   });
 
-var block10AM = $('#hour-10');
+
 var block10 = 10;
 var block10AMTXT = $('#txt10');
 var block10AMBtn = $('btn10AM');
 
-var block11AM = $('#hour-11');
+
 var block11 = 11;
 var block11AMTXT = $('#txt11');
 var block11AMBtn = $('btn11AM');
 
-var block12PM = $('#hour-12');
+
 var block12 = 12;
 var block12PMTXT = $('#txt12');
 var block12PMBtn = $('btn12PM');
 
-var block1PM = $('#hour-13');
+
 var block13 = 13;
 var block12PMTXT = $('#txt12');
 var block12PMBtn = $('btn12PM');
 
-// var block1PM = $('#hour-1');
-// var block14 = 14;
-// var block1PMTXT = $('#txt1');
-// var block1PMBtn = $('btn1PM');
 
-var block2PM = $('#hour-14');
 var block14 = 14;
 var block2PMTXT = $('#txt2');
 var block2PMBtn = $('btn2PM');
 
-var block3PM = $('#hour-15');
 var block15 = 15;
 var block3PMTXT = $('#txt3');
 var block3PMBtn = $('btn3PM');
 
-var block4PM = $('#hour-4');
 var block16 = 16;
 var block4PMTXT = $('#txt4');
 var block4PMBtn = $('btn4PM');
 
-var block5PM = $('#hour-5');
 var block17 = 17;
 var block5PMTXT = $('#txt5');
 var block4PMBtn = $('btn4PM');
 
 
-var idnum = '9' 
+var idnum = 9 
+var hashTag = '#'
+var blockID =  0
+var timeBlock = ''
+// var timeCheck = currentTime;
 
+// currentTime = 15;
+
+// var x = 9;
+// var blockID = hashTag.concat(x);
+//  var timeBlock = $(blockID);
+//  console.log(timeBlock)
+
+//  timeBlock.attr('class', 'row time-block future' );
+
+ console.log(currentTime)
 
 function  colorBlock () {
+  // console.log(idnum)
+  if (currentTime < idnum) {
+    console.log(currentTime)
 
-  if (currentTime < block9) {
+    for (var i = 9; i < 17 ; i++) {
 
-    console.log('ela');
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
 
-    block9AM.attr('class', 'row time-block future' );
-    block10AM.attr('class', 'row time-block future' );
-    block11AM.attr('class', 'row time-block future' );
-    block12PM.attr('class', 'row time-block future' );
-    block1PM.attr('class', 'row time-block future' );
-    block2PM.attr('class', 'row time-block future' );
-    block3PM.attr('class', 'row time-block future' );
-    block4PM.attr('class', 'row time-block future' );
-    block5PM.attr('class', 'row time-block future' );
+  }else if (currentTime === 9) {
 
-    // if (currentTime <= block9) {
-     
-    //   block9AM.attr('class', 'row time-block present' );
-    //   block10AM.attr('class', 'row time-block future' );
-    //   block11AM.attr('class', 'row time-block future' );
-    //   block12PM.attr('class', 'row time-block future' );
-    //   block1PM.attr('class', 'row time-block future' );
-    //   block2PM.attr('class', 'row time-block future' );
-    //   block3PM.attr('class', 'row time-block future' );
-    //   block4PM.attr('class', 'row time-block future' );
-    //   block5PM.attr('class', 'row time-block future' );
-
-    // }
-
-  } else if ((currentTime < block10) || ((currentTime >= block9) )) {
-
-    console.log('ela');
+    console.log("check 9")
     
-    block9AM.attr('class', 'row time-block future' );
-    block10AM.attr('class', 'row time-block future' );
-    block11AM.attr('class', 'row time-block future' );
-    block12PM.attr('class', 'row time-block future' );
-    block1PM.attr('class', 'row time-block future' );
-    block2PM.attr('class', 'row time-block future' );
-    block3PM.attr('class', 'row time-block future' );
-    block4PM.attr('class', 'row time-block future' );
-    block5PM.attr('class', 'row time-block future' );
+    block9AM.attr('class', 'row time-block present')
+    
+    for (i = 10; i < 17 ; i++) {
 
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+    
+  }else if (currentTime  === 10) {
+    console.log("check 10")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block present');
+
+    for (i = 11; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+  }else if (currentTime === 11) {
+    console.log("check 11")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block present');
+
+    for (i = 12; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+  }else if (currentTime === 12) {
+    console.log("check 12")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block past');
+    block12PM.attr('class', 'row time-block present');
+
+    for (i = 13; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+
+  }else if  (currentTime === 13) {
+    console.log("check 13")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block past');
+    block12PM.attr('class', 'row time-block past');
+    block1PM.attr('class', 'row time-block present');
+
+    for (i = 14; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+  }else if (currentTime === 14) {
+    console.log("check 14")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block past');
+    block12PM.attr('class', 'row time-block past');
+    block1PM.attr('class', 'row time-block past');
+    block2PM.attr('class', 'row time-block present');
+
+    for (i = 15; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+  }else if (currentTime === 15) {
+    console.log("check 15")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block past');
+    block12PM.attr('class', 'row time-block past');
+    block1PM.attr('class', 'row time-block past');
+    block2PM.attr('class', 'row time-block past');
+    block3PM.attr('class', 'row time-block present');
+
+    for (i = 16; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+  }
+  if (currentTime === 16) {
+    console.log("check 15")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block past');
+    block12PM.attr('class', 'row time-block past');
+    block1PM.attr('class', 'row time-block past');
+    block2PM.attr('class', 'row time-block past');
+    block3PM.attr('class', 'row time-block past');
+    block4PM.attr('class', 'row time-block present');
+
+    for (i = 16; i < 17 ; i++) {
+
+      blockID = hashTag.concat(i);
+      timeBlock = $(blockID);
+      timeBlock.attr('class', 'row time-block future' );
+      console.log(timeBlock)
+    }
+  }else if (currentTime === 17) {
+    console.log("check 15")
+    block9AM.attr('class', 'row time-block past');
+    block10AM.attr('class', 'row time-block past');
+    block11AM.attr('class', 'row time-block past');
+    block12PM.attr('class', 'row time-block past');
+    block1PM.attr('class', 'row time-block past');
+    block2PM.attr('class', 'row time-block past');
+    block3PM.attr('class', 'row time-block past');
+    block4PM.attr('class', 'row time-block past');
+    block5PM.attr('class', 'row time-block present');
+   
   }
 
-}
+  
+  }
 
 
 
